1c37d07d483e3b4620495f679a09f38e4f5329fb
SuperRun <445679589@qq.com>
2020-04-27 09:51:44 +0800

    联合类型

---
+++ Union Type/index.js
@@ -0,0 +1,8 @@
+// 联合类型
+// + 取值为多个类型中的一种
+var id; // id 可以为number也可以为string
+// + 联合类型取共有的方法
+function getById(id) {
+    // console.log(id.length); // Property 'length' does not exist on type 'number'.
+    console.log(id.toString());
+}
